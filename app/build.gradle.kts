7e2c82e58f3e269f5afcb08d20d9d1a3ea930eed
freetux <thefreetux@gmail.com>
2026-09-20 15:15:09 +0330

    Initial Sarrast WebView app

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.herotux.sarrastwebview"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.herotux.sarrastwebview"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
